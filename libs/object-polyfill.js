8e4eeb8c36dd52d4e683fe7a99e40cb13ca76b48
Patrick G <geek1011@outlook.com>
2018-04-29 14:38:57 -0400

    Added String.prototype.endsWith polyfill for IE

--- libs/object-polyfill.js
+++ libs/object-polyfill.js
@@ -56,3 +56,11 @@ if (!Object.values) Object.values = function (obj) {
         return e[1];
     });
 }
+
+if (!String.prototype.endsWith) String.prototype.endsWith = function(searchString, position) {
+    var subjectString = this.toString();
+    if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) position = subjectString.length;
+    position -= searchString.length;
+    var lastIndex = subjectString.indexOf(searchString, position);
+    return lastIndex !== -1 && lastIndex === position;
+};
