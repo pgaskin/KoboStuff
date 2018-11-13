46c385f9f3bde7645a4ef14bed021be24ad65ea0
Patrick G <geek1011@outlook.com>
2018-11-12 19:03:02 -0500

    Firmware 4.11.11982

--- db.js
+++ db.js
@@ -1512,6 +1512,10 @@ var oldversions = {
         "model": "Kobo Forma",
         "hardware": "kobo7",
         "versions": [{
+            "version": "4.11.11982",
+            "date": "November 2018",
+            "download": "http://download.kobobooks.com/firmwares/kobo7/Nov2018/kobo-update-4.11.11982.zip"
+        }, {
             "version": "4.11.11980",
             "date": "November 2018",
             "download": "http://download.kobobooks.com/firmwares/kobo7/Nov2018/kobo-update-4.11.11980.zip"
